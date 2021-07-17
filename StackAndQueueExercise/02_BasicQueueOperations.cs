d5392df0b53067ffb4174fb266f472c4fec3916a
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-07-17 10:46:57 +0300

    CSharp-Advanced

---
+++ StackAndQueueExercise/02_BasicQueueOperations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = data[0];
+            int s = data[1];
+            int x = data[2];
+
+            int[] numInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Enqueue(numInput[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            if (numbers.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
+        }
+    }
+}
