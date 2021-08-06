6703d189d8dacff444aee9c465fbfe9623f565e4
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-08-06 05:10:46 +0300

    CSharp-Advanced

---
+++ SetsAndDictionariesAdvanced/03_PeriodicTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03_PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countLines = int.Parse(Console.ReadLine());
+            SortedSet<string> chemicals = new SortedSet<string>();
+
+            while (countLines-- > 0)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+
+                foreach (var item in cmdArgs)
+                {
+                    chemicals.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicals));
+        }
+    }
+}
