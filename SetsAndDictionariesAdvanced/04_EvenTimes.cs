1f7df719e5f6ba45f4880e64fb34927f4bf3bc40
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-08-08 12:23:28 +0300

    CSharp-Advanced

---
+++ SetsAndDictionariesAdvanced/04_EvenTimes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(number))
+                {
+                    dict.Add(number, 0);
+                }
+
+                dict[number]++;
+            }
+
+            KeyValuePair<int, int> kvp = dict.First(kvp => kvp.Value % 2 == 0);
+            Console.WriteLine(kvp.Key);
+        }
+    }
+}
