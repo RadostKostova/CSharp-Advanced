82a3319409258f70971189215b7147fa2c801f19
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-08-09 17:52:07 +0300

    CSharp-Advanced

---
+++ SetsAndDictionariesAdvanced/05_CountSymbols.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05_CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            SortedDictionary<char, int> occurancies = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (occurancies.ContainsKey(input[i]) == false)
+                {
+                    occurancies.Add(input[i], 1);
+                }
+                else
+                {
+                    occurancies[input[i]]++;
+                }
+            }
+
+            foreach (var item in occurancies)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
