0e14e5c7910368562dfab83074858d48d4b9afd1
Radost Kostova <75369851+RadostKostova@users.noreply.github.com>
2021-09-07 12:49:31 +0300

    CSharp-Advanced

---
+++ MultidimentionalArrays/01_DiagonalDifference.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    int number = rowData[col]; //rowData[col] is the currElement from the input e.g. rowData
+                    matrix[row, col] = number;
+                }
+            }
+
+            int primaryDiagonal = 0;
+            int counter = n - 1;
+            int secondaryDiagonal = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                primaryDiagonal += matrix[row, row];
+
+                secondaryDiagonal += matrix[row, counter];
+                counter--;
+            }
+         
+            int difference = Math.Abs(primaryDiagonal - secondaryDiagonal);
+            Console.WriteLine(difference);
+        }
+    }
+}
